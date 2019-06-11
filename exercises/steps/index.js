2c56bb6e24bda0498c4cbd375f68595de000619b
Cameron Ray <27529775+upsmancsr@users.noreply.github.com>
2019-06-10 17:01:37 -0700

    steps problem solution 1 complete.

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,8 +17,46 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n) {
+    // loop from 0 to n, for each i print '#' + '' * (n-i) 
+    // if (n < 1) {
+    //     return '';
+    // }
+
+    // let stepsArray = Array(n).fill('');
+
+    // for (let step = 1; step <= n; step++) {
+        
+    //     for (i of stepsArray) {
+    //         stepsArray[i] = '#';
+    //     }
+    //     return stepsArray;
+    // }
+
+    //
+    // Model Solution:
+    //
+
+    // Step through a range from 0 to n for each row to be printed:
+    for (let row = 0; row < n; row++) {
+        // at each row, initialize an empty string called 'stair':
+        let stair = '';
+
+        // at each row, step through the coulumns from 0 to n:
+        for (let column = 0; column < n; column++) {
+            // if column <= row, add a '#' to the stair string,
+            // else, add a space, ' ':
+            if (column <= row) {
+                stair += '#';
+            } else {
+                stair += ' ';
+            }
+        }
+        // console.log stair FOR EACH ROW:
+        console.log(stair);
+
+    }
+
+}
 
 module.exports = steps;
-
-// comment to check new development branch setup
