bee24ba188ea78d319272bd71cbf1297624cab56
Cameron Ray <27529775+upsmancsr@users.noreply.github.com>
2019-06-13 17:53:23 -0700

    anagrams solved using sort()

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -21,47 +21,68 @@
     // Option 1: compare lengths of each character map object
     // Option 2: compare lengths of each word
 
+// ***
+// Solution 1 (character map):
+// ***
+// function anagrams(stringA, stringB) {
+//     // Convert input strings to RegExp:
+//     const stringARegExp = stringA.replace(/[^\w]/g, "").toLowerCase();
+//     const stringBRegExp = stringB.replace(/[^\w]/g, "").toLowerCase();
+//     console.log('stringARegExp:', stringARegExp);
 
+//     // Compare lengths of RegExp strings:
+//     if (stringA.length !== stringB.length) {
+//         return false;
+//     }
+
+//     // Create map objects to count character counts for both input strings:
+//     let stringAMap = {};
+//     for (char of stringARegExp) {
+//         if (stringAMap[char]) {
+//             stringAMap[char]++;
+//         } else {
+//             stringAMap[char] = 1;
+//         }
+//     }
+
+//     let stringBMap = {};
+//     // for (char of stringBRegExp) {
+//     //     if (stringBMap[char]) {
+//     //         stringBMap[char]++;
+//     //     } else {
+//     //         stringBMap[char] = 1;
+//     //     }
+//     // }
+//     // Alternative way to increment character counts:
+//     for (char of stringBRegExp) {
+//         stringBMap[char] = stringBMap[char] + 1 || 1;
+//     }
+
+//     for (char of stringARegExp) {
+//         if (stringAMap[char] !== stringBMap[char]) {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+
+// ***
+// Solution 2 (sort):
+// ***
 function anagrams(stringA, stringB) {
-    // Convert input strings to RegExp:
-    const stringARegExp = stringA.replace(/[^\w]/g, "").toLowerCase();
-    const stringBRegExp = stringB.replace(/[^\w]/g, "").toLowerCase();
-    console.log('stringARegExp:', stringARegExp);
-
-    // Compare lengths of RegExp strings:
-    if (stringA.length !== stringB.length) {
+    // Convert input strings to sorted strings:
+    const stringASorted = sortString(stringA);
+    const stringBSorted = sortString(stringB);
+    console.log('stringASorted:', stringASorted);
+    console.log('stringBSorted:', stringBSorted);
+    if (stringASorted !== stringBSorted) {
         return false;
     }
-
-    // Create map objects to count character counts for both input strings:
-    let stringAMap = {};
-    for (char of stringARegExp) {
-        if (stringAMap[char]) {
-            stringAMap[char]++;
-        } else {
-            stringAMap[char] = 1;
-        }
-    }
-
-    let stringBMap = {};
-    // for (char of stringBRegExp) {
-    //     if (stringBMap[char]) {
-    //         stringBMap[char]++;
-    //     } else {
-    //         stringBMap[char] = 1;
-    //     }
-    // }
-    // Alternative way to increment character counts:
-    for (char of stringBRegExp) {
-        stringBMap[char] = stringBMap[char] + 1 || 1;
-    }
-
-    for (char of stringARegExp) {
-        if (stringAMap[char] !== stringBMap[char]) {
-            return false;
-        }
-    }
     return true;
+}
+
+function sortString(str) {
+    return str.replace(/[^\w]/g, "").toLowerCase().split('').sort().join('');
 }
 
 module.exports = anagrams;
