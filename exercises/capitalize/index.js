e3effc765cbc5f0f5f1ff539c3be7b681fc70254
Cameron Ray <27529775+upsmancsr@users.noreply.github.com>
2019-06-13 19:14:26 -0700

    capitalize solution 1 complete.

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,18 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+    let strArray = str.split('');
+    console.log(strArray);
+    strArray[0] = strArray[0].toUpperCase();
+    for (let i = 0; i < strArray.length; i++) {
+        console.log(strArray[i]);
+        if (strArray[i] === ' ') {
+            strArray[i + 1] = strArray[i + 1].toUpperCase();
+        }
+    }
+    console.log(strArray.join(''));
+    return strArray.join('');
+}
 
 module.exports = capitalize;
