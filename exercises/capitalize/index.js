4d6fc3b3e02b06c7d5fb2698823dbc4387fc3838
Cameron Ray <27529775+upsmancsr@users.noreply.github.com>
2019-06-13 21:10:07 -0700

    capitalize solution 2 with slice complete.

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,18 +7,39 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
+// ***
+// Solution 1:
+// ***
+// function capitalize(str) {
+//     let strArray = str.split('');
+//     console.log(strArray);
+//     strArray[0] = strArray[0].toUpperCase();
+//     for (let i = 0; i < strArray.length; i++) {
+//         console.log(strArray[i]);
+//         if (strArray[i] === ' ') {
+//             strArray[i + 1] = strArray[i + 1].toUpperCase();
+//         }
+//     }
+//     console.log(strArray.join(''));
+//     return strArray.join('');
+// }
+
+// ***
+// Solution 2 (slice):
+// ***
 function capitalize(str) {
-    let strArray = str.split('');
-    console.log(strArray);
-    strArray[0] = strArray[0].toUpperCase();
-    for (let i = 0; i < strArray.length; i++) {
-        console.log(strArray[i]);
-        if (strArray[i] === ' ') {
-            strArray[i + 1] = strArray[i + 1].toUpperCase();
-        }
+    // split input string at each space, creating an array of arrays,
+    // where each array is a word
+    let wordsArray = str.split(' ');
+    console.log(wordsArray);
+    let capitalWordsArray = [];
+
+    for (let word of wordsArray) {
+        capitalWordsArray.push(word[0].toUpperCase() + word.slice(1));
+        console.log(word);
     }
-    console.log(strArray.join(''));
-    return strArray.join('');
+    console.log(capitalWordsArray.join(' '));
+    return capitalWordsArray.join(' ');
 }
 
 module.exports = capitalize;
