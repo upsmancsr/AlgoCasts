2e06e4b8175b731dcf5f9871e2caccf64327ee2a
Cameron Ray <27529775+upsmancsr@users.noreply.github.com>
2019-06-16 18:54:46 -0700

    vowels solution 2 complete.

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,18 +7,31 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
+// // ***
+// // Solution 1:
+// // ***
+// function vowels(str) {
+//     // Create an array containging all vowels:
+//     const vowels = ['a', 'e', 'i', 'o', 'u'];
+//     // Initialize a count starting at 0 to count all vowels in str:
+//     let vowelCount = 0;
+//     // Loop through all characters in str and increment str when a vowel is found:
+//     for (let char of str) {
+//         if (vowels.includes(char.toLowerCase())) {
+//             vowelCount++;
+//         }
+//     }
+//     return vowelCount;
+// }
+
+// ***
+// Solution 2 (match() and RegExp):
+// ***
 function vowels(str) {
-    // Create an array containging all vowels:
-    const vowels = ['a', 'e', 'i', 'o', 'u'];
-    // Initialize a count starting at 0 to count all vowels in str:
-    let vowelCount = 0;
-    // Loop through all characters in str and increment str when a vowel is found:
-    for (let char of str) {
-        if (vowels.includes(char.toLowerCase())) {
-            vowelCount++;
-        }
-    }
-    return vowelCount;
+
+    const matches = str.match(/[aeiou]/gi)
+
+    return matches ? matches.length : 0;
 }
 
 module.exports = vowels;
