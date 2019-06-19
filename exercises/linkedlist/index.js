9213dfa12f8c4e3f35a1a5df0c1ca0a0a4e5b176
Cameron Ray <27529775+upsmancsr@users.noreply.github.com>
2019-06-18 20:27:46 -0700

    linkedlist Node class constructor built.

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -2,7 +2,17 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+// Note on direction of list and "next" convention:
+// "next" points in descending order, e.g., given nodes n1 and n2, n2.next is n1, and n1.next is null
+
+
+class Node {
+    // constructor function is called with 2 args, 'data' and 'next':
+    constructor(data, next) {
+        this.data = data;
+        this.next = next;
+    }
+}
 
 class LinkedList {}
 
