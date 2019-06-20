5272e23369c926d454c8da5a3dfe7080c13477e4
Cameron Ray <27529775+upsmancsr@users.noreply.github.com>
2019-06-20 01:08:13 -0700

    for...of loop test passed with Symbol.iterator generator function.

--- exercises/linkedlist/test.js
+++ exercises/linkedlist/test.js
@@ -324,7 +324,7 @@ describe('ForEach', () => {
   });
 });
 
-describe.skip('for...of loops', () => {
+describe('for...of loops', () => {
   test('works with the linked list', () => {
     const l = new List();
 
