93de413eb08caf52d837d20bccf647ced08bd42d
Cameron Ray <27529775+upsmancsr@users.noreply.github.com>
2019-06-20 22:07:19 -0700

    contains method completed.

--- exercises/bst/index.js
+++ exercises/bst/index.js
@@ -21,7 +21,7 @@ class Node {
     // insert method to create and insert a node in the tree in relation to the Node itself
     // ie, in relation to this.data:
     insert(data) {
-        
+
         // data could be less than or greater than this.data
         // if this.left exists, data could be less than or greater than this.left.data
         // if this.right exists, data could be less than or greater than this.right.data
@@ -32,16 +32,32 @@ class Node {
                 this.left.insert(data);
             } else {
                 this.left = new Node(data);
-                return;
+                // return;
             }
         } else {
             if (this.right) {
                 this.right.insert(data);
             } else {
                 this.right = new Node(data);
-                return;
+                // return;
             }
         }
+    }
+
+    // contains method takes data as arg and returns node with equal data value if it exists:
+    contains(data) {
+
+        if (data === this.data) {
+            return this;
+        }
+        
+        if (data < this.data && this.left) {
+            return this.left.contains(data);
+        } else if (data > this.data && this.right) {
+            return this.right.contains(data);
+        } 
+
+        return null;
     }
 }
 
