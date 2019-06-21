6fa4ce157bf5110ed9ba3d34346a67faf86544f6
Cameron Ray <27529775+upsmancsr@users.noreply.github.com>
2019-06-20 20:03:17 -0700

    BST Node.insert method completed.

--- exercises/bst/index.js
+++ exercises/bst/index.js
@@ -11,6 +11,38 @@
 // class.  Contains should accept a 'data' argument
 // and return the Node in the tree with the same value.
 
-class Node {}
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+
+    // insert method to create and insert a node in the tree in relation to the Node itself
+    // ie, in relation to this.data:
+    insert(data) {
+        
+        // data could be less than or greater than this.data
+        // if this.left exists, data could be less than or greater than this.left.data
+        // if this.right exists, data could be less than or greater than this.right.data
+        // etc...
+
+        if (data < this.data) {
+            if (this.left) {
+                this.left.insert(data);
+            } else {
+                this.left = new Node(data);
+                return;
+            }
+        } else {
+            if (this.right) {
+                this.right.insert(data);
+            } else {
+                this.right = new Node(data);
+                return;
+            }
+        }
+    }
+}
 
 module.exports = Node;
