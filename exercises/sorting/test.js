6368da31d9184aaca87592a509c1d18a4f6c5bfc
Cameron Ray <27529775+upsmancsr@users.noreply.github.com>
2019-06-24 23:49:11 -0700

    selectionSort function completed.

--- exercises/sorting/test.js
+++ exercises/sorting/test.js
@@ -18,7 +18,7 @@ describe('Bubble sort', () => {
   });
 });
 
-describe.skip('Selection sort', () => {
+describe('Selection sort', () => {
   test('sorts an array', () => {
     expect(selectionSort(getArray())).toEqual(getSortedArray());
   });
